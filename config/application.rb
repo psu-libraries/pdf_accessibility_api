94e565c073f9231e11e8deb5fbf8f833f73ec1d8
Jesse Landis-Eigsti <jml8735@psu.edu>
2025-06-24 08:53:09 -0600

    Adds Sidekiq

--- config/application.rb
+++ config/application.rb
@@ -28,13 +28,8 @@ module PdfAccessibilityApi
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
 
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    # ActiveJob config
+    config.active_job.queue_adapter = :sidekiq
 
     # Don't generate system test files.
     config.generators.system_tests = nil
---
+++ config/initializers/sidekiq.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+Sidekiq.configure_server do |config|
+end
+
+Sidekiq.configure_client do |config|
+end
