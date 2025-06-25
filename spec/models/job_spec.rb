131592e8598b1ccd6ebba057ed0efedbb67a092d
Eric Durante <eric@westarete.com>
2025-06-25 13:30:04 -0400

    Infer spec types from file location to make Rubocop happy

--- spec/models/job_spec.rb
+++ spec/models/job_spec.rb
@@ -2,7 +2,7 @@
 
 require 'rails_helper'
 
-RSpec.describe Job, type: :model do
+RSpec.describe Job do
   describe 'table' do
     it { is_expected.to have_db_column(:id).of_type(:integer).with_options(null: false) }
     it { is_expected.to have_db_column(:uuid).of_type(:string).with_options(null: false) }
