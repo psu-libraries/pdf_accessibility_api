350144b112cd775a78fd72152e95cb474b5498ae
Eric Durante <eric@westarete.com>
2025-07-08 16:32:13 -0400

    Timeout the file remediation jobs if no output is produced
    Also, notify clients of failures as well as success.

--- app/jobs/remediation_job.rb
+++ app/jobs/remediation_job.rb
@@ -1,21 +1,26 @@
 # frozen_string_literal: true
 
 class RemediationJob < ApplicationJob
-  def perform(job_uuid)
+  OUTPUT_POLLING_INTERVAL = 10 # This value was picked somewhat arbitrarily. We may want to adjust.
+
+  # The default 1-hour timeout is also arbitrary and should probably be adjusted.
+  def perform(job_uuid, output_polling_timeout = 3600)
     job = Job.find_by!(uuid: job_uuid)
     tempfile = Down.download(job.source_url)
     object_key = "#{SecureRandom.hex(8)}_#{tempfile.original_filename}"
     s3 = S3Handler.new(object_key)
     s3.upload_to_input(tempfile.path)
 
-    until output_url = s3.presigned_url_for_output
-      sleep 10 # This value was picked somewhat arbitrarily. We may want to adjust.
+    timer = 0
 
-      # We'll probably want this to time out eventually and record a failure if the
-      # output file isn't found after some reasonably long period of time, but I don't
-      # think that we know how long we should wait yet. Also, I don't yet know if there
-      # is any other way to detect a file remediation failure apart from the absence of
-      # an output file. If there is, then we'll want to check for that.
+    until output_url = s3.presigned_url_for_output
+      sleep OUTPUT_POLLING_INTERVAL
+      timer += OUTPUT_POLLING_INTERVAL
+
+      if timer > output_polling_timeout
+        record_failure_and_notify(job, 'Timed out waiting for output file')
+        return true
+      end
     end
 
     job.update(
@@ -28,21 +33,23 @@ class RemediationJob < ApplicationJob
     RemediationStatusNotificationJob.perform_later(job_uuid)
   rescue Down::Error => e
     # We may want to retry the download depending on the more specific nature of the failure.
-    record_failure(job, "Failed to download file from source URL:  #{e.message}")
+    record_failure_and_notify(job, "Failed to download file from source URL:  #{e.message}")
   rescue S3Handler::Error => e
     # We may want to retry the upload depending on the more specific nature of the failure.
-    record_failure(job, "Failed to upload file to remediation input location:  #{e.message}")
+    record_failure_and_notify(job, "Failed to upload file to remediation input location:  #{e.message}")
   ensure
     tempfile&.close!
   end
 
   private
 
-    def record_failure(job, message)
+    def record_failure_and_notify(job, message)
       job.update(
         status: 'failed',
         finished_at: Time.zone.now,
         processing_error_message: message
       )
+
+      RemediationStatusNotificationJob.perform_later(job.uuid)
     end
 end
