177353076819438eecbafbca5dbbc1186edac483
Alex Kiessling <ajkiessl@gmail.com>
2025-07-31 16:53:32 -0400

    Adds rswag-api back in and it's configurations. Regenerate swagger yaml

--- Gemfile
+++ Gemfile
@@ -38,6 +38,7 @@ gem 'faraday'
 
 # RSwag for API documentation
 gem 'rswag-ui'
+gem 'rswag-api'
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ require 'sidekiq/web'
 require 'sidekiq_web_constraint'
 
 Rails.application.routes.draw do
+  mount Rswag::Api::Engine => '/api-docs'
   mount Rswag::Ui::Engine => '/api-docs'
   mount Sidekiq::Web => '/sidekiq', :constraints => SidekiqWebConstraint.new
   get '/sidekiq', to: ->(_env) {
