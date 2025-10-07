bf351d28a566dbb17709e1e693a77071a73cdb29
Jesse Landis-Eigsti <jml8735@psu.edu>
2025-10-07 08:59:14 -0600

    158 - rubocop

--- app/models/image_job.rb
+++ app/models/image_job.rb
@@ -3,8 +3,9 @@
 class ImageJob < Job
   belongs_to :owner, polymorphic: true
 
-private
-  def broadcast_to_job_channel
-    # To be implemented in #160
-  end
+  private
+
+    def broadcast_to_job_channel
+      # To be implemented in #160
+    end
 end
