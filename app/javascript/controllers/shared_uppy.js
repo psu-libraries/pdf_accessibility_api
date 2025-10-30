378ed38105a6886a2bfe7f930a85881b4ef1c6a2
Jesse Landis-Eigsti <jml8735@psu.edu>
2025-10-30 09:06:21 -0600

    193 - removes unneeded 'g' from RegEx

--- app/javascript/controllers/shared_uppy.js
+++ app/javascript/controllers/shared_uppy.js
@@ -1,5 +1,5 @@
 export function checkForForbiddenCharacters(files) {
-    let forbidden_chars = /[^A-Za-z0-9\.\-_ ]/g
+    let forbidden_chars = /[^A-Za-z0-9\.\-_ ]/
     if (files[0].name.match(forbidden_chars) != null) {
       let message = 'File names can only contain letters, numbers, spaces, periods, underscores, and hyphens.'
       alert(message)
