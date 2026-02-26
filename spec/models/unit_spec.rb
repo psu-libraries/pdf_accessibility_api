d252a0892d27f1be03b086c9739f707067ff5ff4
Alex Kiessling <ajkiessl@gmail.com>
2026-02-26 15:52:42 -0500

    Changes units daily_page_limit field to user_daily_page_limit to clarify that the daily limit is per user, not for the whole unit

--- spec/models/unit_spec.rb
+++ spec/models/unit_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Unit do
   describe 'table' do
     it { is_expected.to have_db_column(:id).of_type(:integer).with_options(null: false) }
     it { is_expected.to have_db_column(:name).of_type(:string) }
-    it { is_expected.to have_db_column(:daily_page_limit).of_type(:integer).with_options(null: false, default: 30) }
+    it { is_expected.to have_db_column(:user_daily_page_limit).of_type(:integer).with_options(null: false, default: 30) }
     it { is_expected.to have_db_column(:overall_page_limit).of_type(:integer).with_options(null: false, default: 25000) }
     it { is_expected.to have_db_column(:created_at).of_type(:datetime).with_options(null: false) }
     it { is_expected.to have_db_column(:updated_at).of_type(:datetime).with_options(null: false) }
@@ -23,7 +23,7 @@ RSpec.describe Unit do
 
   describe 'validations' do
     it { is_expected.to validate_presence_of(:name) }
-    it { is_expected.to validate_presence_of(:daily_page_limit) }
+    it { is_expected.to validate_presence_of(:user_daily_page_limit) }
     it { is_expected.to validate_presence_of(:overall_page_limit) }
   end
 
