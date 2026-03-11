29ea95155a297834e0f529d67e7ac81606bdcf4c
Alex Kiessling <ajkiessl@gmail.com>
2026-03-11 17:06:42 -0400

    Backoff/throttle S3 checks a bit more so we aren't churning like crazy when we don't need to

--- app/services/check_s3_for_final_files_service.rb
+++ app/services/check_s3_for_final_files_service.rb
@@ -5,7 +5,7 @@
 class CheckS3ForFinalFilesService
   include AppJobModule
 
-  CHECK_S3_PER_JOB_INTERVAL = 0.5 # Throttle S3 checks while iterating processing jobs
+  CHECK_S3_PER_JOB_INTERVAL = 1 # Throttle S3 checks while iterating processing jobs
   CHECK_S3_IDLE_INTERVAL = 10 # Back off when there are no processing jobs
   CHECK_S3_FAILED_LIMIT = 3600 # 1 hour limit before marking job as failed
 
@@ -46,6 +46,9 @@ class CheckS3ForFinalFilesService
       end
 
       break if run_once || stop_requested
+
+      # Back off after each loop
+      sleep CHECK_S3_IDLE_INTERVAL
     ensure
       ActiveRecord::Base.connection_handler.clear_active_connections!
     end
