301a536bc7af2b257247009a191feffb66b559e1
Jesse Landis-Eigsti <jml8735@psu.edu>
2026-03-20 14:00:30 -0600

    128 - fixes broken test

--- spec/features/pdf_jobs/show_spec.rb
+++ spec/features/pdf_jobs/show_spec.rb
@@ -9,6 +9,7 @@ RSpec.feature 'PDF Jobs show', :js do
       source_url: 'https://example.com/file1.pdf',
       object_key: 'file1.pdf',
       filename: 'file1.pdf',
+      uuid: 'abc123',
       created_at: Time.new(2024, 7, 22, 10, 30),
       finished_at: Time.new(2024, 7, 22, 11, 0, 0, '-04:00'),
       status: 'completed',
