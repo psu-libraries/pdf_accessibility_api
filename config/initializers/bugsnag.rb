d883b39d70fd358fade2b63aa86d450edc8a8b15
Olawale Jebutu <okj5043@psu.edu>
2026-04-22 14:11:18 -0400

    Add error handling to Bugsnag configuration for specific actions

--- config/initializers/bugsnag.rb
+++ config/initializers/bugsnag.rb
@@ -4,4 +4,10 @@ Bugsnag.configure do |config|
   config.app_version = ENV.fetch('APP_VERSION', nil)
   config.release_stage = ENV.fetch('BUGSNAG_RELEASE_STAGE', 'development')
   config.api_key = ENV.fetch('BUGSNAG_API_KEY', nil)
+
+  config.add_on_error(proc do |event|
+    action = event.request&.dig(:railsAction)
+
+    event.ignore! if action&.start_with?('ok_computer')
+  end)
 end
